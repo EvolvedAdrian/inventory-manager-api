fcfd2e10d0d41423612a552e76cf399c88194c76
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 01:21:20 +0200

    feat: add category entity

---
+++ src/main/java/com/evolvedadrian/inventory/entity/Category.java
@@ -0,0 +1,33 @@
+package com.evolvedadrian.inventory.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    public Category() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
