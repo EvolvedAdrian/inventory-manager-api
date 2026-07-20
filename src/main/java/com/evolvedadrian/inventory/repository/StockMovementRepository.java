6855c0d5d6653eb9c93efa1a6d213a2cfb24c7b0
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 20:27:25 +0200

    feat: add new find methods to stock movement service

--- src/main/java/com/evolvedadrian/inventory/repository/StockMovementRepository.java
+++ src/main/java/com/evolvedadrian/inventory/repository/StockMovementRepository.java
@@ -1,7 +1,20 @@
 package com.evolvedadrian.inventory.repository;
 
+import com.evolvedadrian.inventory.entity.Product;
 import com.evolvedadrian.inventory.entity.StockMovement;
+import com.evolvedadrian.inventory.entity.Warehouse;
+import com.evolvedadrian.inventory.enums.MovementType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface StockMovementRepository extends JpaRepository<StockMovement, Integer> {
+    List<StockMovement> findByType(MovementType type);
+
+    List<StockMovement> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    List<StockMovement> findByProduct(Product product);
+
+    List<StockMovement> findByWarehouse(Warehouse warehouse);
 }
--- src/main/java/com/evolvedadrian/inventory/service/StockMovementService.java
+++ src/main/java/com/evolvedadrian/inventory/service/StockMovementService.java
@@ -1,9 +1,13 @@
 package com.evolvedadrian.inventory.service;
 
+import com.evolvedadrian.inventory.entity.Product;
 import com.evolvedadrian.inventory.entity.StockMovement;
+import com.evolvedadrian.inventory.entity.Warehouse;
+import com.evolvedadrian.inventory.enums.MovementType;
 import com.evolvedadrian.inventory.repository.StockMovementRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +32,7 @@ public class StockMovementService {
     }
 
     public StockMovement updateStockMovement(StockMovement stockMovement) {
-        if(getStockMovementById(stockMovement.getId()).isEmpty()){
+        if (getStockMovementById(stockMovement.getId()).isEmpty()) {
             throw new RuntimeException("Stock movement does not exist.");
         }
         return this.stockMovementRepository.save(stockMovement);
@@ -36,5 +40,21 @@ public class StockMovementService {
 
     public void deleteStockMovement(StockMovement stockMovement) {
         this.stockMovementRepository.delete(stockMovement);
+    }
+
+    public List<StockMovement> findStockMovementsByType(MovementType type) {
+        return this.stockMovementRepository.findByType(type);
+    }
+
+    public List<StockMovement> findStockMovementsByDateBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return this.stockMovementRepository.findByDateBetween(startDate, endDate);
+    }
+
+    public List<StockMovement> findStockMovementsByProduct(Product product) {
+        return this.stockMovementRepository.findByProduct(product);
+    }
+
+    public List<StockMovement> findStockMovementsByWarehouse(Warehouse warehouse) {
+        return this.stockMovementRepository.findByWarehouse(warehouse);
     }
 }
