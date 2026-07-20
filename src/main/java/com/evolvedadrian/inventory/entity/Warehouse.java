f8585e61b435eee30746350d15314f30a1cd17f4
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 10:13:51 +0200

    feat: add warehouse entity

---
+++ src/main/java/com/evolvedadrian/inventory/entity/Warehouse.java
@@ -0,0 +1,42 @@
+package com.evolvedadrian.inventory.entity;
+
+import jakarta.persistence.*;
+
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 255)
+    private String location;
+
+    public Warehouse() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
