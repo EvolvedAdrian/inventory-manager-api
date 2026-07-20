906cc5db40a06130ce68a6572167da79c220e3d3
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 10:10:08 +0200

    feat: add supplier entity

---
+++ src/main/java/com/evolvedadrian/inventory/entity/Supplier.java
@@ -0,0 +1,53 @@
+package com.evolvedadrian.inventory.entity;
+
+import jakarta.persistence.*;
+
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(unique = true, length = 150)
+    private String email;
+
+    @Column(length = 30)
+    private String phone;
+
+    public Supplier() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
