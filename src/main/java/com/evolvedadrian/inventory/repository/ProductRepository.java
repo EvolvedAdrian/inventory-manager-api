b30612dfb3edbb14cb020f5f33a76cf8d64d28ad
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 20:44:28 +0200

    feat: add new find methods to product service

--- src/main/java/com/evolvedadrian/inventory/repository/ProductRepository.java
+++ src/main/java/com/evolvedadrian/inventory/repository/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.evolvedadrian.inventory.repository;
 
+import com.evolvedadrian.inventory.entity.Category;
 import com.evolvedadrian.inventory.entity.Product;
+import com.evolvedadrian.inventory.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Optional<Product> findBySku(String sku);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByCategory(Category category);
+
+    List<Product> findBySupplier(Supplier supplier);
+
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }
--- src/main/java/com/evolvedadrian/inventory/service/ProductService.java
+++ src/main/java/com/evolvedadrian/inventory/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.evolvedadrian.inventory.service;
 
+import com.evolvedadrian.inventory.entity.Category;
 import com.evolvedadrian.inventory.entity.Product;
+import com.evolvedadrian.inventory.entity.Supplier;
 import com.evolvedadrian.inventory.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,5 +46,21 @@ public class ProductService {
 
     public Optional<Product> findProductBySku(String sku) {
         return this.productRepository.findBySku(sku);
+    }
+
+    public List<Product> findProductsByName(String name){
+        return this.productRepository.findByName(name);
+    }
+
+    public List<Product> findProductsByCategory(Category category){
+        return this.productRepository.findByCategory(category);
+    }
+
+    public List<Product> findProductsBySupplier(Supplier supplier){
+        return this.productRepository.findBySupplier(supplier);
+    }
+
+    public List<Product> findProductsByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice){
+        return this.productRepository.findByPriceBetween(minPrice, maxPrice);
     }
 }
