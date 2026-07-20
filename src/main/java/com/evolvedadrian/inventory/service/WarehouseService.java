07e3de96b2331c5f7db9b233943ac4015dfada96
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-20 18:09:06 +0200

    feat: add warehouse service

---
+++ src/main/java/com/evolvedadrian/inventory/service/WarehouseService.java
@@ -0,0 +1,37 @@
+package com.evolvedadrian.inventory.service;
+
+import com.evolvedadrian.inventory.entity.Warehouse;
+import com.evolvedadrian.inventory.repository.WarehouseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WarehouseService {
+    private final WarehouseRepository warehouseRepository;
+
+    public WarehouseService(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    public List<Warehouse> getAllWarehouses() {
+        return this.warehouseRepository.findAll();
+    }
+
+    public Optional<Warehouse> getWarehouseById(Integer id) {
+        return this.warehouseRepository.findById(id);
+    }
+
+    public Warehouse createWarehouse(Warehouse warehouse) {
+        return this.warehouseRepository.save(warehouse);
+    }
+
+    public Warehouse updateWarehouse(Warehouse warehouse) {
+        return this.warehouseRepository.save(warehouse);
+    }
+
+    public void deleteWarehouse(Warehouse warehouse) {
+        this.warehouseRepository.delete(warehouse);
+    }
+}
